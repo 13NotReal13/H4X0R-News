045f106fcd22f5827bfc995ba3ba0092ffa0e7c9
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-23 17:55:11 +0200

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Иван Семикин on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
